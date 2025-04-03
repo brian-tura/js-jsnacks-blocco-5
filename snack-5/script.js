155c7f3d83325e2b1acb98d97e28f5c438a7c020
brian-tura <b.tura2002@gmail.com>
2025-04-03 09:17:04 +0200

    [UPD] Snack 5 Done

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,11 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+const even = nums.filter(isEven);
+console.log(even);
+
+function isEven(numbers){
+    return numbers % 2 == 0;
+}
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
