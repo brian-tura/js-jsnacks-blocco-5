162880129c40ddc767691dccfbd5d68f08eb6bd9
brian-tura <b.tura2002@gmail.com>
2025-04-03 09:07:26 +0200

    [UPD] Snack 2 done

--- snack-2/script.js
+++ snack-2/script.js
@@ -4,5 +4,10 @@ const people = [
   { name: 'Marco', age: 67 }
 ];
 
+for(let i=0; i<people.length; i++){
+  console.log(people[i].name);
+}
+
+
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
